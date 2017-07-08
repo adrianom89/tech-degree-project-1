046b137e846b0ad44002a75e86258b7b38ae2736
adrianom89 <adrianom@mac.com>
2017-07-08 14:39:47 -0400

    Updated the Collection requirement for Part 1: Using a Dictionary to store the information for each player

--- SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator/SoccerCoordinator.playground/Contents.swift
@@ -1,5 +1,30 @@
-//: Playground - noun: a place where people can play
+////**Part 1** - Collections requirement
 
-import UIKit
+//Dictionary that stores information for each player's name and Height
+let playerNameAndHeight = [
+    
+    "Joe Smith": 42,
+    "Jill Tanner": 36,
+    "Bill Bon": 43,
+    "Eva Gordon": 45,
+    "Matt Gill": 40,
+    "Kimmy Stein": 41,
+    "Sammy Adams": 45,
+    "Karl Saygan": 43,
+    "Suzane Greenberg": 44,
+    "Sal Dali": 41,
+    "Joe Kavalier": 39,
+    "Ben Finkelstein": 44,
+    "Diego Soto": 41,
+    "Chloe Alaska": 47,
+    "Arnold Willis": 43,
+    "Phillip Helm": 44,
+    "Les Clay": 42,
+    "Herschel Krustofski": 45
+]
 
-var str = "Hello, playground"
+
+
+
+//Empty Collection variable fot all
+
